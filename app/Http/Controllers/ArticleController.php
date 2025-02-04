f7fa6f2aef8bcedd1e8e49e21a652754ef21f213
MouhamedAbbassi <mouhamed.abbassi@esprit.tn>
2025-02-04 17:08:08 +0100

    removing unsed files

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -22,61 +22,6 @@ class ArticleController extends Controller
         return Article::findOrFail($id);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        $request->validate([
-            'Title' => 'required',
-            'Image' => 'required',
-            'HeaderImage' => 'required',
-            'Introduction' => 'required',
-            'Description' => 'required',
-            'Language' => 'required',
-            'KeyWords' => 'required',
-            'State' => 'required',
-            'NumVisit' => 'required',
-            'IdTheme' => 'required',
-            'IdUser' => 'required',
-            'IdHost' => 'required',
-        ]);
 
-        return Article::create($request->all());
-    }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        $request->validate([
-            'Title' => 'required',
-            'Image' => 'required',
-            'HeaderImage' => 'required',
-            'Introduction' => 'required',
-            'Description' => 'required',
-            'Language' => 'required',
-            'KeyWords' => 'required',
-            'State' => 'required',
-            'NumVisit' => 'required',
-            'IdTheme' => 'required',
-            'IdUser' => 'required',
-            'IdHost' => 'required',
-        ]);
-
-        $article = Article::findOrFail($id);
-        $article->update($request->all());
-
-        return $article;    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        Article::findOrFail($id)->delete();
-
-        return response()->noContent();
-    }
 }
